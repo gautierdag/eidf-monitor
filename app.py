1950ec9636e064b9f0efc0d9066602f42064d7d2
Gautier Dagan <gautier.dagan@ed.ac.uk>
2024-03-26 11:05:21 +0000

    more stats and aggregate metrics over last hour

--- app.py
+++ app.py
@@ -15,19 +15,33 @@ The data is collected from the Kubernetes cluster every 5 minutes.
 Active GPUs are those with memory usage greater than 1% in the last 5 minutes.
 """)
 
-def get_data()->pd.DataFrame:
+
+def get_data() -> pd.DataFrame:
     df = pd.read_json(FILE_PATH)
+
+    def add_gpu_id(gpu_usage):
+        new_list = []
+        for i, g in enumerate(gpu_usage):
+            g["gpu_id"] = i
+            new_list.append(g)
+        return new_list
+
+    df["gpu_usage"] = df["gpu_usage"].apply(add_gpu_id)
     df = df.explode("gpu_usage")
-    df = pd.concat([df, df.gpu_usage.apply(pd.Series)], axis=1).drop("gpu_usage", axis=1)
+    df = pd.concat([df, df.gpu_usage.apply(pd.Series)], axis=1).drop(
+        "gpu_usage", axis=1
+    )
     df["gpu_mem_used"] = df["memory_used"] / df["memory_total"] * 100
     df["inactive"] = df["gpu_mem_used"] < 1
     return df
 
-def get_colors(df: pd.DataFrame)->dict:
+
+def get_colors(df: pd.DataFrame) -> dict:
     gpu_names = set(df["gpu_name"].unique())
     colors = px.colors.qualitative.Plotly
     color_map = {gpu_name: colors[i] for i, gpu_name in enumerate(gpu_names)}
     return color_map
+
 
 df = get_data()
 color_map = get_colors(df)
@@ -36,79 +50,173 @@ color_map = get_colors(df)
 latest_timestamp = df["timestamp"].max()
 current_df = df[df["timestamp"] == latest_timestamp].copy()
 
-current_usage_df = current_df.groupby(["username", "gpu_name"]).agg({"gpu_name": "count", "inactive": "sum", "memory_free": "sum", "pod_name": list}).rename(columns={"gpu_name": "count"}).reset_index()
+# show current global counts
+gpu_counts = current_df.gpu_name.value_counts()
+cols = st.columns(len(gpu_counts))
+for col, (gpu_name, count) in zip(cols, gpu_counts.items()):
+    with col:
+        st.metric(
+            f"Active {gpu_name}",
+            count,
+            delta=None,
+            delta_color="normal",
+            help=None,
+            label_visibility="visible",
+        )
+
+
+st.data_editor(
+    df.groupby("pod_name")
+    .agg({"username": "first", "gpu_mem_used": list})
+    .reset_index(),
+    column_config={
+        "gpu_mem_used": st.column_config.LineChartColumn(
+            "GPU Memory Utilization (%)",
+            # width="medium",
+            help="Memory utilization of the GPUs in the pod over time",
+            y_min=0,
+            y_max=100,
+        ),
+    },
+    hide_index=True,
+    use_container_width=True,
+)
+
+# aveerage user usage in last hour
+last_hour_df = df[df["timestamp"] > latest_timestamp - pd.Timedelta(hours=1)]
+last_hour_df = (
+    last_hour_df.groupby(["pod_name", "gpu_id"])
+    .agg(
+        {
+            "memory_free": "mean",
+            "gpu_mem_used": "mean",
+            "gpu_name": "first",
+            "inactive": "all",
+            "username": "first",
+        }
+    )
+    .reset_index()
+)
+
+last_hour_usage_df = (
+    last_hour_df.groupby(["username", "gpu_name"])
+    .agg(
+        {
+            "gpu_name": "count",
+            "inactive": "sum",
+            "memory_free": "sum",
+            "pod_name": list,
+            "gpu_mem_used": "mean",
+        }
+    )
+    .rename(columns={"gpu_name": "count"})
+    .reset_index()
+)
 # count total as well
-current_usage_df["count_total"] = current_usage_df.groupby("username")["count"].transform("sum")
-current_usage_df["count_total_inactive"] = current_usage_df.groupby("username")["inactive"].transform("sum")
+last_hour_usage_df["count_total"] = last_hour_usage_df.groupby("username")[
+    "count"
+].transform("sum")
+last_hour_usage_df["count_total_inactive"] = last_hour_usage_df.groupby("username")[
+    "inactive"
+].transform("sum")
 
-current_usage_df["memory_free"] = current_usage_df["memory_free"] / 1024 
-current_usage_df["memory_free_total"] = current_usage_df.groupby("username")["memory_free"].transform("sum")
+last_hour_usage_df["memory_free"] = last_hour_usage_df["memory_free"] / 1024
+last_hour_usage_df["memory_free_total"] = last_hour_usage_df.groupby("username")[
+    "memory_free"
+].transform("sum")
 
-current_usage_df["pod_name"] = current_usage_df["pod_name"].apply(lambda x: ", ".join(set(x)))
+last_hour_usage_df["pod_name"] = last_hour_usage_df["pod_name"].apply(
+    lambda x: ", ".join(set(x))
+)
 
 # plot current usage per user
-sorted_df = current_usage_df.sort_values(by="count_total", ascending=False)
-fig = px.bar(current_usage_df, 
-             x="username", 
-             y="count", 
-             color="gpu_name", 
-             title="Current GPU usage per user",
-             color_discrete_map=color_map,
-             category_orders={'username': sorted_df['username'].tolist()},
-             hover_data={"pod_name": True},
-             labels={"count": "Number of GPUs"})
+sorted_df = last_hour_usage_df.sort_values(by="count_total", ascending=False)
+fig = px.bar(
+    last_hour_usage_df,
+    x="username",
+    y="count",
+    color="gpu_name",
+    title="GPU usage per user (last hour)",
+    color_discrete_map=color_map,
+    category_orders={"username": sorted_df["username"].tolist()},
+    hover_data={"pod_name": True},
+    labels={"count": "Number of GPUs"},
+)
 
 st.plotly_chart(fig, use_container_width=True)
 
 # plot current inactive GPUs per user
-sorted_df = current_usage_df.sort_values(by="count_total_inactive", ascending=False)
-fig = px.bar(current_usage_df[current_usage_df["inactive"] > 0],
-                x="username",
-                y="inactive",
-                title="Current inactive GPUs per user",
-                color="gpu_name",
-                color_discrete_map=color_map,
-                category_orders={'username': sorted_df['username'].tolist()},
-                hover_data={"pod_name": True},
-                labels={"inactive": "Number of Inactive GPUs"})
+sorted_df = last_hour_usage_df.sort_values(by="count_total_inactive", ascending=False)
+fig = px.bar(
+    last_hour_usage_df[last_hour_usage_df["inactive"] > 0],
+    x="username",
+    y="inactive",
+    title="Inactive GPUs per user (last hour)",
+    color="gpu_name",
+    color_discrete_map=color_map,
+    category_orders={"username": sorted_df["username"].tolist()},
+    hover_data={"pod_name": True},
+    labels={"inactive": "Number of Inactive GPUs"},
+)
 st.plotly_chart(fig, use_container_width=True)
 
 
 # chart of memory free per user
-sorted_df = current_usage_df.sort_values(by="memory_free_total", ascending=False)
+sorted_df = last_hour_usage_df.sort_values(by="memory_free_total", ascending=False)
 
-fig = px.bar(current_usage_df,
-                x="username",
-                y="memory_free",
-                title="Current Total GPU Memory free per user",
-                color="gpu_name",
-                color_discrete_map=color_map,
-                category_orders={'username': sorted_df['username'].tolist()},
-                hover_data={"pod_name": True},
-                labels={"memory_free": "Memory free (GB)"})
+fig = px.bar(
+    last_hour_usage_df,
+    x="username",
+    y="memory_free",
+    title="Total GPU Memory free per user (last hour)",
+    color="gpu_name",
+    color_discrete_map=color_map,
+    category_orders={"username": sorted_df["username"].tolist()},
+    hover_data={"pod_name": True},
+    labels={"memory_free": "Memory free (GB)"},
+)
 st.plotly_chart(fig, use_container_width=True)
 
 
-
 # plot average utilization rates
-avg_usage_df = df.groupby(["username", "gpu_name"]).agg({"gpu_mem_used": "mean"}).reset_index()
-sorted_df = avg_usage_df.sort_values(by="gpu_mem_used", ascending=False)
-fig = px.bar(avg_usage_df, 
-             x="username", 
-             y="gpu_mem_used", 
-             color="gpu_name", 
-             title="Average GPU memory usage per user",
-             color_discrete_map=color_map,
-             barmode="group",
-             category_orders={'username': sorted_df['username'].tolist()},
-             labels={"gpu_mem_used": "Average GPU Memory Utilization (%)"})
+avg_usage_df = (
+    df.groupby(["username", "gpu_name"]).agg({"gpu_mem_used": "mean"}).reset_index()
+)
+sorted_df = last_hour_usage_df.sort_values(by="gpu_mem_used", ascending=False)
+fig = px.bar(
+    last_hour_usage_df,
+    x="username",
+    y="gpu_mem_used",
+    color="gpu_name",
+    title="Average GPU memory usage per user",
+    color_discrete_map=color_map,
+    barmode="group",
+    category_orders={"username": sorted_df["username"].tolist()},
+    labels={"gpu_mem_used": "Average GPU Memory Utilization (%)"},
+)
 st.plotly_chart(fig, use_container_width=True)
 
 
 # plot GPU usage over time per user
-gpu_usage_df = df.groupby(["username", "timestamp"]).agg({"gpu_name": "count", "inactive": "sum"}).reset_index()
-fig = px.line(gpu_usage_df, x="timestamp", y="gpu_name", color="username", title="GPU usage over time per user")
+gpu_usage_df = (
+    df.groupby(["username", "timestamp"])
+    .agg({"gpu_name": "count", "inactive": "sum"})
+    .reset_index()
+)
+fig = px.line(
+    gpu_usage_df,
+    x="timestamp",
+    y="gpu_name",
+    color="username",
+    title="GPU usage over time per user",
+)
 st.plotly_chart(fig, use_container_width=True)
 
-fig = px.line(gpu_usage_df[gpu_usage_df["inactive"] > 0], x="timestamp", y="inactive", color="username", title="Inactive GPUs over time per user")
+fig = px.line(
+    gpu_usage_df[gpu_usage_df["inactive"] > 0],
+    x="timestamp",
+    y="inactive",
+    color="username",
+    title="Inactive GPUs over time per user",
+)
 st.plotly_chart(fig, use_container_width=True)
--- utils.py
+++ utils.py
@@ -4,58 +4,65 @@ from datetime import datetime
 from kubernetes import client, config
 from kubernetes.stream import stream
 
+
 def get_pods_info():
     cmd = "kubectl get pods -n informatics -o json"
     result = subprocess.run(cmd.split(), capture_output=True, text=True)
     return json.loads(result.stdout)
 
+
 def get_pods_command():
     pods_info = get_pods_info()
     pod_cmd, pod_runtime, pod_numgpus = {}, {}, {}
-    for item in pods_info['items']:
-        pod_name = item['metadata']['name']
-        containers = item['spec']['containers']
+    for item in pods_info["items"]:
+        pod_name = item["metadata"]["name"]
+        containers = item["spec"]["containers"]
 
         try:
-            start_time = datetime.fromisoformat(item['status']['startTime'].rstrip("Z"))
+            start_time = datetime.fromisoformat(item["status"]["startTime"].rstrip("Z"))
             runtime_duration = str(datetime.now() - start_time)
-        except: 
-            runtime_duration = '-1.'
+        except:
+            runtime_duration = "-1."
 
         total_cpu = 0
         total_gpu = 0
-        command = 'None'
+        command = "None"
 
         for container in containers:
-            resources = container.get('resources', {})
-            requests = resources.get('requests', {})
-            limits = resources.get('limits', {})
+            resources = container.get("resources", {})
+            requests = resources.get("requests", {})
+            limits = resources.get("limits", {})
 
-            cpu = requests.get('cpu', "0")
-            gpu = limits.get('nvidia.com/gpu', "0")
+            cpu = requests.get("cpu", "0")
+            gpu = limits.get("nvidia.com/gpu", "0")
 
-            total_cpu += int(cpu[:-1]) if 'm' in cpu else int(cpu) * 1000
+            total_cpu += int(cpu[:-1]) if "m" in cpu else int(cpu) * 1000
             total_gpu += int(gpu)
-            if 'command' in container:
-                command = ' '.join(container['command'])
-            if 'args' in container:
-                command += ' '
-                command += ' '.join(container['args'])
+            if "command" in container:
+                command = " ".join(container["command"])
+            if "args" in container:
+                command += " "
+                command += " ".join(container["args"])
         pod_cmd.update({pod_name: command})
         pod_runtime.update({pod_name: runtime_duration})
         pod_numgpus.update({pod_name: total_gpu})
     return pod_cmd, pod_runtime, pod_numgpus
-        
+
+
 def filter_while_true_pods():
     pod_cmd, pod_runtime, pod_numgpus = get_pods_command()
     while_true_pods = []
     for k, v in pod_cmd.items():
-        if 'sleep infinity' in v or 'while true' in v:
-            while_true_pods.append({
-                'name': k, 'command': v, 'runtime': pod_runtime[k], '#GPUs': pod_numgpus[k]
-            })
+        if "sleep infinity" in v or "while true" in v:
+            while_true_pods.append(
+                {
+                    "name": k,
+                    "command": v,
+                    "runtime": pod_runtime[k],
+                    "#GPUs": pod_numgpus[k],
+                }
+            )
     return while_true_pods
-
 
 
 def get_pods_not_using_gpus(namespace: str = "informatics") -> list[dict]:
@@ -73,7 +80,10 @@ def get_pods_not_using_gpus(namespace: str = "informatics") -> list[dict]:
         if pod.status.phase != "Running":
             continue
         # check if the pod is using GPUs
-        if not any("nvidia.com/gpu" in container.resources.limits for container in pod.spec.containers):
+        if not any(
+            "nvidia.com/gpu" in container.resources.limits
+            for container in pod.spec.containers
+        ):
             continue
 
         # Command to count the number of GPUs
@@ -83,15 +93,27 @@ def get_pods_not_using_gpus(namespace: str = "informatics") -> list[dict]:
 
         try:
             # Execute commands in the pod
-            gpu_count = stream(v1.connect_get_namespaced_pod_exec, pod.metadata.name, namespace,
-                                command=['/bin/sh', '-c', gpu_count_cmd],
-                                stderr=True, stdin=False,
-                                stdout=True, tty=False)
+            gpu_count = stream(
+                v1.connect_get_namespaced_pod_exec,
+                pod.metadata.name,
+                namespace,
+                command=["/bin/sh", "-c", gpu_count_cmd],
+                stderr=True,
+                stdin=False,
+                stdout=True,
+                tty=False,
+            )
 
-            gpu_memories = stream(v1.connect_get_namespaced_pod_exec, pod.metadata.name, namespace,
-                                    command=['/bin/sh', '-c', gpu_mem_cmd],
-                                    stderr=True, stdin=False,
-                                    stdout=True, tty=False)
+            gpu_memories = stream(
+                v1.connect_get_namespaced_pod_exec,
+                pod.metadata.name,
+                namespace,
+                command=["/bin/sh", "-c", gpu_mem_cmd],
+                stderr=True,
+                stdin=False,
+                stdout=True,
+                tty=False,
+            )
 
             # Process the outputs
             num_gpus = int(gpu_count.strip())
@@ -99,9 +121,9 @@ def get_pods_not_using_gpus(namespace: str = "informatics") -> list[dict]:
             if all(mem < 100 for mem in gpu_memories):
                 if num_gpus > 0:
                     entry = {
-                        'pod': pod.metadata.name,
-                        'namespace': namespace,
-                        'num_gpus': num_gpus
+                        "pod": pod.metadata.name,
+                        "namespace": namespace,
+                        "num_gpus": num_gpus,
                     }
                     res += [entry]
 
@@ -110,7 +132,7 @@ def get_pods_not_using_gpus(namespace: str = "informatics") -> list[dict]:
     return res
 
 
-def get_gpu_usage_in_pod(pod_name, namespace="informatics")->list[dict]:
+def get_gpu_usage_in_pod(pod_name, namespace="informatics") -> list[dict]:
     # Create a Kubernetes API client
     v1 = client.CoreV1Api()
 
@@ -118,23 +140,32 @@ def get_gpu_usage_in_pod(pod_name, namespace="informatics")->list[dict]:
     gpu_mem_cmd = "nvidia-smi --query-gpu=gpu_name,memory.used,memory.free,memory.total,utilization.gpu,utilization.memory --format=csv,noheader,nounits"
 
     try:
-        gpu_memories = stream(v1.connect_get_namespaced_pod_exec, pod_name, namespace,
-                                command=['/bin/sh', '-c', gpu_mem_cmd],
-                                stderr=True, stdin=False,
-                                stdout=True, tty=False)
+        gpu_memories = stream(
+            v1.connect_get_namespaced_pod_exec,
+            pod_name,
+            namespace,
+            command=["/bin/sh", "-c", gpu_mem_cmd],
+            stderr=True,
+            stdin=False,
+            stdout=True,
+            tty=False,
+        )
 
         # Process the outputs
         # num_gpus = int(gpu_count.strip())
         gpu_memories = [x for x in gpu_memories.split("\n") if x]
         gpu_memories = [x.split(",") for x in gpu_memories]
-        gpu_memories = [{
-            'gpu_name': x[0],
-            'memory_used': int(x[1].split()[0]),
-            'memory_free': int(x[2].split()[0]),
-            'memory_total': int(x[3].split()[0]),
-            'gpu_util': int(x[4].split()[0]),
-            'memory_util': int(x[5].split()[0])
-        } for x in gpu_memories]
+        gpu_memories = [
+            {
+                "gpu_name": x[0],
+                "memory_used": int(x[1].split()[0]),
+                "memory_free": int(x[2].split()[0]),
+                "memory_total": int(x[3].split()[0]),
+                "gpu_util": int(x[4].split()[0]),
+                "memory_util": int(x[5].split()[0]),
+            }
+            for x in gpu_memories
+        ]
 
         return gpu_memories
 
@@ -143,7 +174,7 @@ def get_gpu_usage_in_pod(pod_name, namespace="informatics")->list[dict]:
         return []
 
 
-def get_pods_not_using_gpus_stats(namespace = "informatics")->list[dict]:
+def get_pods_not_using_gpus_stats(namespace="informatics") -> list[dict]:
     config.load_kube_config("/kubernetes/config")
 
     # Create a Kubernetes API client
@@ -158,19 +189,24 @@ def get_pods_not_using_gpus_stats(namespace = "informatics")->list[dict]:
         if pod.status.phase != "Running":
             continue
         # check if the pod is using GPUs
-        if not any("nvidia.com/gpu" in container.resources.limits for container in pod.spec.containers):
+        if not any(
+            "nvidia.com/gpu" in container.resources.limits
+            for container in pod.spec.containers
+        ):
             continue
-        
+
         pod_name = pod.metadata.name
         username = pod.metadata.labels["eidf/user"].replace("-infk8s", "")
         pod_id = pod.metadata.uid
         gpu_usage = get_gpu_usage_in_pod(pod_name)
         if len(gpu_usage) == 0:
             continue
-        data.append({
-            "pod_name": pod_name,
-            "username": username,
-            "pod_id": pod_id,
-            "gpu_usage": gpu_usage
-        })
+        data.append(
+            {
+                "pod_name": pod_name,
+                "username": username,
+                "pod_id": pod_id,
+                "gpu_usage": gpu_usage,
+            }
+        )
     return data
