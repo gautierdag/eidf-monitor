d7fe2ff244542806284b0941f1e98b703c912157
Gautier Dagan <gautier.dagan@ed.ac.uk>
2024-03-26 11:05:43 +0000

    lint cron file

--- cron.py
+++ cron.py
@@ -6,33 +6,41 @@ from utils import get_pods_not_using_gpus_stats
 
 FILE_PATH = "/nfs/user/s2234411-infk8s/cluster_gpu_usage.json"
 
+
 def load_data():
     try:
-        with open(FILE_PATH, 'r') as file:
+        with open(FILE_PATH, "r") as file:
             return json.load(file)
     except FileNotFoundError:
         return []
 
+
 def save_data(data):
-    with open(FILE_PATH, 'w') as file:
+    with open(FILE_PATH, "w") as file:
         json.dump(data, file, indent=4)
+
 
 def main():
     new_data_list = get_pods_not_using_gpus_stats()
-    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     for data in new_data_list:
-        data['timestamp'] = timestamp
+        data["timestamp"] = timestamp
 
     existing_data = load_data()
     # Append the new data
     existing_data.extend(new_data_list)
     # Filter out data older than 31 days
     cutoff = datetime.now() - timedelta(days=31)
-    filtered_data = [entry for entry in existing_data if datetime.strptime(entry['timestamp'], '%Y-%m-%d %H:%M:%S') > cutoff]
-    
+    filtered_data = [
+        entry
+        for entry in existing_data
+        if datetime.strptime(entry["timestamp"], "%Y-%m-%d %H:%M:%S") > cutoff
+    ]
+
     save_data(filtered_data)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     print(f"Running main function at {datetime.now()}")
     main()
